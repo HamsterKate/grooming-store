07b0652ca1382cc8305a5f76020fb6702ceef460
Kate <hamster.k.po@gmail.com>
2026-05-31 15:29:00 +0300

    refactor grooming_settings folder to store_settings

--- store_settings/urls.py
+++ store_settings/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for grooming_settings project.
+URL configuration for store_settings project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
