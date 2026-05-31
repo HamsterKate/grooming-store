a6cec8ffa0b3e0582ccc50862a9cf10e24c31e05
Kate <hamster.k.po@gmail.com>
2026-05-31 15:51:55 +0300

    set index page and routs for it

--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from store.models import Groomer, Service, Pet
+
+
 # Create your views here.
+def index(request):
+    num_groomers = Groomer.objects.count()
+    num_services = Service.objects.count()
+    num_pets = Pet.objects.count()
+
+    context = {
+        "num_groomers": num_groomers,
+        "num_services": num_services,
+        "num_pets": num_pets,
+    }
+    return render(
+        request,
+        "store/index.html",
+        context=context,
+    )
