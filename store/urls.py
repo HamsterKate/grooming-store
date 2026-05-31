b2d3ebcd4d997319b7e18f5c8a29972a5f213e6a
Kate <hamster.k.po@gmail.com>
2026-05-31 19:02:38 +0300

    add list-view urls to store urls.py

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,7 @@ from store import views
 app_name = "store"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("groomers/", views.GroomerView.as_view(), name="groomers"),
+    path("pets/", views.PetView.as_view(), name="pets"),
+    path("services/", views.ServiceView.as_view(), name="services"),
 ]
