b8596eada6e10aa062ffdecd682ae00c6c5fd703
Kate <hamster.k.po@gmail.com>
2026-06-01 18:51:57 +0300

    add groomer_detail.html + path groomer-detail

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,7 @@ app_name = "store"
 urlpatterns = [
     path("", views.index, name="index"),
     path("groomers/", views.GroomerView.as_view(), name="groomer-list"),
+    path("groomers/<int:pk>/", views.GroomerDetailView.as_view(), name="groomer-detail"),
     path("pets/", views.PetView.as_view(), name="pet-list"),
     path("services/", views.ServiceView.as_view(), name="service-list"),
 ]
