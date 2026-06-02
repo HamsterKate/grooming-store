bf5674fc96d20c7e4f109bfbb0937f31d40fe555
Kate <hamster.k.po@gmail.com>
2026-06-02 23:39:38 +0300

    move auth URLs to project routing

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from store import views
 
 app_name = "store"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("groomers/", views.GroomerListView.as_view(), name="groomer-list"),
@@ -11,5 +12,5 @@ urlpatterns = [
     path("pets/<int:pk>/", views.PetDetailView.as_view(), name="pet-detail"),
     path("services/", views.ServiceListView.as_view(), name="service-list"),
     path("services/<int:pk>/", views.ServiceDetailView.as_view(), name="service-detail"),
-    path("accounts/", include("django.contrib.auth.urls"), name="accounts"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
