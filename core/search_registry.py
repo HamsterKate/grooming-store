bb2632479e47572952c679b82f1f9bfa6d11d05f
Kate <hamster.k.po@gmail.com>
2026-06-02 20:33:35 +0300

    add service search configuration to registry

--- core/search_registry.py
+++ core/search_registry.py
@@ -4,10 +4,17 @@ SEARCH_FIELDS = {
         ("last_name", "Last name"),
         ("qualification", "Qualification"),
     ],
+    "service-list": [
+        ("name", "Name"),
+        ("type", "Type"),
+    ],
 }
 
 FIELD_LOOKUP = {
     "first_name": "first_name__icontains",
     "last_name": "last_name__icontains",
     "qualification": "qualifications__name__icontains",
+
+    "name": "name__icontains",
+    "type": "type__icontains",
 }
